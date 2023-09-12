19a8ba9cb957c59453fe2eaeb570fdcce0e302a9
vinayakbabbar <babbarvinayak7@gmail.com>
2023-09-12 14:56:54 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import './App-responsive.css'
+import Navigation from './components/Navigation'
+import HeroSection from './components/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navigation />
+    <HeroSection/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,33 @@
+import {MdOutlineMenu} from 'react-icons/md'
+
+
+
+const Navigation=()=>{
+
+return(
+    <nav className="container">
+        <div className='logo'>
+            <img src="/images/brand_logo.png" alt="logo"/>
+        </div>
+
+        <div className="list">
+          <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+          </ul>
+        </div>
+
+        <div className="login">
+          <button >Login</button>
+        </div>
+
+        <MdOutlineMenu className='menu'/>
+
+      </nav>
+)
+
+}
+
+export default Navigation
